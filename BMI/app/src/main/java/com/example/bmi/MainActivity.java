4d6f1179b7b35a6c8c85aa26657750a19e5cd061
Laiii151 <eve021282@gmail.com>
2024-03-21 23:15:37 +0800

    113-03-21

--- BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -16,34 +17,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txvShow = (TextView) findViewById(R.id.txvShow);
+        txvShow = (TextView)findViewById(R.id.txvShow);
         txvShow.setTextSize(36);
         Button btnCalc = (Button) findViewById(R.id.btnCalc);
-        Button btnClear = (Button) findViewById(R.id.btnClar);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
         btnCalc.setOnClickListener(this);
         btnClear.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        EditText editHeight = (EditText) findViewById(R.id.editHeight);
-        EditText editWeight = (EditText) findViewById(R.id.editWeight);
+        EditText edtHeight = (EditText) findViewById(R.id.edtHeight);
+        EditText edtWeight = (EditText) findViewById(R.id.edtWeight);
+
         if(v.getId() == R.id.btnCalc){
-            double height = Double.parseDouble(editHeight.getText().toString());
-            double weight = Double.parseDouble(editWeight.getText().toString());
-            double bmi = weight / Math.pow(height/100.0, 2);
-            if(bmi >= 24) {
+            double height = Double.parseDouble(edtHeight.getText().toString());
+            double Weight = Double.parseDouble(edtWeight.getText().toString());
+            double bmi = Weight / Math.pow(height/100.0, 2);
+            if (bmi >= 24)
                 txvShow.setTextColor(Color.RED);
-            }else if(bmi < 18.5) {
+            else if (bmi < 18.5)
                 txvShow.setTextColor(Color.BLUE);
-            }else {
+            else
                 txvShow.setTextColor(Color.GREEN);
-            }
+
             txvShow.setText(String.format("%.2f", bmi));
         }
-        else{
-            editHeight.setText("0");
-            editWeight.setText("0");
+        else {
+            edtHeight.setText("0");
+            edtWeight.setText("0");
             txvShow.setText("");
         }
     }
