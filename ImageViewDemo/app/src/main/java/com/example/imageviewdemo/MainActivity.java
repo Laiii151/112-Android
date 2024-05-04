2edd41a913c4dc113e729f73e52f9405c9df9864
Laiii151 <eve021282@gmail.com>
2024-05-04 17:38:02 +0800

    113-05-04

--- ImageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
+++ ImageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
@@ -7,26 +7,41 @@ import android.widget.ImageView;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView image;
+    private ImageView image1, image2, image3, image4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = (ImageView) findViewById(R.id.imgOutput1);
+        image1 = (ImageView) findViewById(R.id.imgOutput1);
+        image2 = (ImageView) findViewById(R.id.imgOutput2);
+        image3 = (ImageView) findViewById(R.id.imgOutput3);
+        image4 = (ImageView) findViewById(R.id.imgOutput4);
         RadioGroup rg = (RadioGroup) findViewById(R.id.rgImages);
 
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if(checkedId == R.id.rdbDog){
-                    image.setImageResource(R.mipmap.dog);
+                    image1.setVisibility(ImageView.VISIBLE);
+                    image2.setVisibility(ImageView.GONE);
+                    image3.setVisibility(ImageView.GONE);
+                    image4.setVisibility(ImageView.GONE);
                 }else if(checkedId == R.id.rdbElephant){
-                    image.setImageResource(R.mipmap.elephant);
+                    image1.setVisibility(ImageView.GONE);
+                    image2.setVisibility(ImageView.VISIBLE);
+                    image3.setVisibility(ImageView.GONE);
+                    image4.setVisibility(ImageView.GONE);
                 }else if(checkedId == R.id.rdbMouse){
-                    image.setImageResource(R.mipmap.mouse);
+                    image1.setVisibility(ImageView.GONE);
+                    image2.setVisibility(ImageView.GONE);
+                    image3.setVisibility(ImageView.VISIBLE);
+                    image4.setVisibility(ImageView.GONE);
                 }else if(checkedId == R.id.rdbRabbit){
-                    image.setImageResource(R.mipmap.rabbit);
+                    image1.setVisibility(ImageView.GONE);
+                    image2.setVisibility(ImageView.GONE);
+                    image3.setVisibility(ImageView.GONE);
+                    image4.setVisibility(ImageView.VISIBLE);
                 }
             }
         });
